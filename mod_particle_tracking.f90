50eb60594922dd6125f954bd3b0cb47633fead16
blackcata <blackcata109@gmail.com>
2019-01-29 15:31:07 +0900

    Add the particle tracking module file

---
+++ mod_particle_tracking.f90
@@ -0,0 +1,23 @@
+!------------------------------------------------------------------------------!
+!                                                                              !
+!   PROGRAM : mod_particle_tracking.f90                                        !
+!                                                                              !
+!   PURPOSE : Module for tracking the particle's trajectories and attributes   !
+!                                                                              !
+!                                                             2019.01.29 K.Noh !
+!                                                                              !
+!------------------------------------------------------------------------------!
+
+
+        MODULE particle_tracking
+
+          USE kinds
+          IMPLICIT NONE
+
+          INTEGER(iwp) ::  unit_par, num_par
+
+          SAVE
+
+        CONTAINS
+
+        END MODULE
