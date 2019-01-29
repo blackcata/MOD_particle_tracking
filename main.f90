866453ae9af98bea5bf9b720988c743602c55fd6
blackcata <blackcata109@gmail.com>
2019-01-29 15:50:15 +0900

    Add the particle number reading function

--- main.f90
+++ main.f90
@@ -14,5 +14,11 @@
           USE particle_tracking
 
           IMPLICIT NONE
+          CHARACTER(LEN=100) :: path, filename
+
+          path      = '/home/km109/PALM_60/JOBS/NP_test/OUTPUT/PARTICLE_DATA/'
+          filename  = 'particle_list.dat' 
+          
+          PRINT*,READ_PAR_NUM(path,filename)          
 
         END PROGRAM main
--- mod_particle_tracking.f90
+++ mod_particle_tracking.f90
@@ -19,5 +19,21 @@
           SAVE
 
         CONTAINS
+          FUNCTION READ_PAR_NUM(path,filename)
+            INTEGER(iwp) ::  READ_PAR_NUM, unit_par, io
+            REAL(wp)     ::  tmp
+            CHARACTER(LEN=100),INTENT(IN) :: path, filename
+
+            unit_par = 200
+            OPEN(unit_par,FILE=TRIM(path)//TRIM(filename),STATUS='OLD')
+
+            READ_PAR_NUM = 0
+
+            DO 
+              READ(unit_par,*,IOSTAT=io) tmp
+              IF (io < 0 ) EXIT
+              READ_PAR_NUM = READ_PAR_NUM + 1
+            END DO 
+          END FUNCTION READ_PAR_NUM
 
         END MODULE
